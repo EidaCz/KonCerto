87ab5b74cd8f3e65654ed7b27bf4ed45d92ba037
EidaCz <eida@eida.cz>
2023-11-18 01:01:14 +0100

    basic logic for providing custom Records

--- src/main/java/cz/eida/minecraft/koncerto/record/Record.java
+++ src/main/java/cz/eida/minecraft/koncerto/record/Record.java
@@ -1,4 +1,192 @@
 package cz.eida.minecraft.koncerto.record;
 
+import cz.eida.minecraft.koncerto.KonCerto;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.checkerframework.checker.units.qual.C;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * Disc representation.
+ *
+ * @author EidaCz
+ */
 public class Record {
+
+    private KonCerto plugin;
+
+    private final Material BASE_DISC = Material.MUSIC_DISC_11;
+
+
+    /** disc namespace */
+    private String namespace;
+    /** disc namespaced key */
+    private NamespacedKey nsk;
+    /** disc CustomModelData ID, defaults to 1000 */
+    private Integer model = 1000;
+    /** disc can be dropped by Creeper, defaults to true */
+    private boolean drop = true;
+    /** disc title */
+    private String title;
+    /** disc author */
+    private String author;
+    /** disc custom lore, multiple lines */
+    private List<String> lore = new ArrayList<>();
+
+    // TODO probability settings
+    // TODO RealisticSeasons settings
+
+    public Record() {
+    }
+
+    /**
+     * Full constructor.
+     *
+     * @param namespace disc namespace
+     * @param model custom model data ID
+     * @param drop droppable by creepers
+     * @param title disc title
+     * @param author disc author
+     * @param lore custom lore lines
+     */
+    public Record(KonCerto plugin, String namespace, Integer model, boolean drop, String title, String author, List<String> lore) {
+
+        this.plugin = plugin;
+
+        this.namespace = namespace;
+        this.nsk = new NamespacedKey(this.plugin, namespace);
+
+        this.model = model;
+        this.drop = drop;
+        this.title = title;
+        this.author = author;
+        this.lore = lore;
+    }
+
+    /**
+     * Simplified constructor.
+     *
+     * @param plugin plugin instance
+     * @param namespace disc namespace
+     * @param title disc title
+     * @param author disc author
+     */
+    public Record(KonCerto plugin, String namespace, String title, String author) {
+
+        this.plugin = plugin;
+
+        this.namespace = namespace;
+        this.nsk = new NamespacedKey(this.plugin, namespace);
+
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getNamespace() {
+        return namespace;
+    }
+
+    public Integer getModel() {
+        return model;
+    }
+
+    public boolean isDrop() {
+        return drop;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public List<String> getLore() {
+        return lore;
+    }
+
+    public List<Component> getLoreComponent() {
+        List<Component> lore = new ArrayList<>();
+        for (String loreLine : this.lore) {
+            lore.add(Component.text(loreLine));
+        }
+        return lore;
+    }
+
+    public void setNamespace(String namespace) {
+        this.namespace = namespace;
+        this.nsk = new NamespacedKey(this.plugin, namespace);
+    }
+
+    public void setModel(Integer model) {
+        this.model = model;
+    }
+
+    public void setDrop(boolean drop) {
+        this.drop = drop;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setLore(List<String> lore) {
+        this.lore = lore;
+    }
+
+    public ItemStack getItem() {
+
+        if (plugin.isSupported_KotatkovyKlub()) {
+            // TODO - simplified item generation
+        }
+
+        ItemStack discItem = new ItemStack(BASE_DISC, 1);
+        ItemMeta discMeta = discItem.getItemMeta();
+
+        discMeta.setCustomModelData(this.model);
+        discMeta.displayName(Component.text(this.author + " - " + this.title));
+
+        if (this.lore != null) {
+            discMeta.lore(getLoreComponent());
+        }
+
+        discItem.setItemMeta(discMeta);
+
+        return discItem;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder info = new StringBuilder();
+
+        info.append("Record");
+        info.append(" { ");
+        info.append("namespace=" + getNamespace());
+        info.append(", ");
+        info.append("author=" + getAuthor());
+        info.append(", ");
+        info.append("title=" + getTitle());
+        info.append(", ");
+        info.append("model=" + getModel());
+        info.append(", ");
+        info.append("droppable=" + isDrop());
+        info.append(", ");
+        info.append("lore=" + getLore());
+        info.append(", ");
+        info.append("item=" + getItem().toString());
+        info.append(" } ");
+
+        return info.toString();
+    }
 }
--- src/main/java/cz/eida/minecraft/koncerto/record/RecordProvider.java
+++ src/main/java/cz/eida/minecraft/koncerto/record/RecordProvider.java
@@ -1,33 +1,90 @@
 package cz.eida.minecraft.koncerto.record;
 
-import org.bukkit.configuration.InvalidConfigurationException;
+import cz.eida.minecraft.koncerto.KonCerto;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
 
+
 import java.io.File;
-import java.io.IOException;
+import java.util.*;
+import java.util.logging.Level;
 
 /**
  * Record database.
  *
  * @author EidaCz
  */
-public class RecordProvider extends YamlConfiguration {
+public class RecordProvider {
 
-    private final String records = "records.yml";
     private Plugin plugin;
+
+    private static RecordProvider recordProvider;
+
+    private final String recordDataFile = "records.yml";
+    private YamlConfiguration recordDataSource;
+    private Map<Integer, Record> records;
 
     public RecordProvider(Plugin plugin) {
         this.plugin = plugin;
+        reloadDatabase();
     }
 
     public void reloadDatabase() {
-        try {
-            load(new File(plugin.getDataFolder(), records));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (InvalidConfigurationException e) {
-            throw new RuntimeException(e);
+
+        File data = new File(plugin.getDataFolder(), recordDataFile);
+
+        recordDataSource = YamlConfiguration.loadConfiguration(data);
+
+        if (!data.exists()) {
+            try {
+                recordDataSource.save(data);
+            } catch (Exception e) {
+                plugin.getLogger().log(Level.SEVERE, "Could not save record file.");
+            }
         }
+
+        records = new HashMap<>();
+        int recordCounter = 0;
+
+        for (ListIterator<Map<?,?>> rec = recordDataSource.getMapList("records").listIterator(); rec.hasNext(); ) {
+
+            Map<String,?> internalData = (Map<String,?>) rec.next();
+            Record record = new Record(
+                    (KonCerto) plugin,
+                    (String) internalData.get("namespace"),
+                    (Integer) internalData.get("model"),
+                    (Boolean) internalData.get("drop"),
+                    (String) internalData.get("title"),
+                    (String) internalData.get("author"),
+                    (List<String>) internalData.get("lore")
+            );
+
+            records.put(recordCounter, record);
+            recordCounter++;
+        }
+    }
+
+    public static RecordProvider getInstance(Plugin plugin) {
+
+        if (recordProvider == null) {
+            recordProvider = new RecordProvider(plugin);
+        }
+
+        return recordProvider;
+    }
+
+    public Record getRandom() {
+
+        if (records.size() == 0) {
+            return null;
+        }
+
+        Random random = new Random();
+        return records.get(random.nextInt(records.size()));
+    }
+
+    // TODO ?
+    public static Record recordFactory() {
+        return null;//return new Record();
     }
 }
